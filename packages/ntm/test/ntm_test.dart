bc70f23c0e6ee4b14762bb5c7cb8573a0ccfaedc
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-08-25 00:03:12 +0800

    :white_check_mark: Add ntm test

--- packages/ntm/test/ntm_test.dart
+++ packages/ntm/test/ntm_test.dart
@@ -1,1 +1,38 @@
+import 'dart:io';
 
+import 'package:mocktail/mocktail.dart';
+import 'package:ntm/ntm.dart';
+import 'package:test/test.dart';
+
+class _MockStdout extends Mock implements Stdout {}
+
+void main() {
+  test('It should evaluate 1 == 1 to true', () {
+    final stdout = _MockStdout();
+    final stderr = _MockStdout();
+    IOOverrides.runZoned(
+      () {
+        Ntm().run('1 == 1');
+      },
+      stdout: () => stdout,
+      stderr: () => stdout,
+    );
+
+    verify(() => stdout.writeln(true)).called(1);
+    verifyNever(() => stderr.writeln(any()));
+  });
+  test('It should evaluate 1 != 1 to false', () {
+    final stdout = _MockStdout();
+    final stderr = _MockStdout();
+    IOOverrides.runZoned(
+      () {
+        Ntm().run('1 != 1');
+      },
+      stdout: () => stdout,
+      stderr: () => stdout,
+    );
+
+    verify(() => stdout.writeln(false)).called(1);
+    verifyNever(() => stderr.writeln(any()));
+  });
+}
