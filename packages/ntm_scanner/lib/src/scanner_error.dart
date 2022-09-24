836a934037e16287199bd9d5b7fcfda9f1e69060
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-09-24 23:21:57 +0800

    :memo: Add doc to scanner

--- packages/ntm_scanner/lib/src/scanner_error.dart
+++ packages/ntm_scanner/lib/src/scanner_error.dart
@@ -1,17 +1,39 @@
+import 'package:equatable/equatable.dart';
 import 'package:ntm_core/ntm_core.dart';
 
-class ScannerError extends DescribableError {
+/// An error that occurred during the scan of a ntm text file.
+class ScannerError extends DescribableError with EquatableMixin {
+  /// An error that occurred during the scan of a ntm text file.
   const ScannerError({
     required this.line,
     required this.column,
     required this.message,
   });
+
+  /// The line of the error.
   final int line;
+
+  /// The column of the error.
   final int column;
+
+  /// The error message.
   final String message;
 
   @override
   String describe() {
     return '[$line:$column]: $message';
   }
+
+  @override
+  String toString() {
+    return '''
+ScannerError(
+  line: $line,
+  column: $column,
+  message: $message,
+)''';
+  }
+
+  @override
+  List<Object?> get props => [line, column, message];
 }
