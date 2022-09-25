f94c1e1d7e5605e0ee581a6340f68d58b51971e8
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-09-25 19:35:02 +0800

    :memo: Scanner - Add example

--- packages/ntm_scanner/example/ntm_scanner_example.dart
+++ packages/ntm_scanner/example/ntm_scanner_example.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'package:ntm_scanner/ntm_scanner.dart';
+
+void main() {
+  final source = r"print 'Hello World';";
+  final scanner = Scanner(source: source);
+  final scanResult = scanner.scanTokens();
+  print(scanResult);
+}
