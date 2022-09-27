1435fc39a57193669c817cf41af6ebcc06cb1bdc
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-09-27 22:45:14 +0800

    :white_check_mark: Add a test for ntm_cli

--- packages/ntm_cli/test/ntm_cli_test.dart
+++ packages/ntm_cli/test/ntm_cli_test.dart
@@ -1,16 +1,16 @@
-import 'package:ntm_cli/ntm_cli.dart';
 import 'package:test/test.dart';
+import 'package:test_process/test_process.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+  test('It should run the ntm file', () async {
+    final process = await TestProcess.start(
+      'dart',
+      const ['run', 'ntm_cli', 'test/test.ntm'],
+    );
+    final stdoutList = <String>[];
+    while (await process.stdout.hasNext) {
+      stdoutList.add(await process.stdout.next);
+    }
+    expect(stdoutList, orderedEquals(const ['Hello World']));
   });
 }
